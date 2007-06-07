e41d3b4447ee6cdd854ae4f8439ced7f8f09e803
Rob Church <robchurch@users.mediawiki.org>
2007-06-07 19:02:57 +0000

    Fix up the oncoming i18n trainwreck here;
    * Move translations to a .i18n.php file as with other extensions
    * Use English as a fallback (as before), but if a translation is available for some (or all) magic words, use that, i.e. fall back to English when a translation isn't available for a given word, to avoid breakage when a new one is introduced

---
+++ ParserFunctions.i18n.php.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Get translated magic words, if available
+ *
+ * @param string $lang Language code
+ * @return array
+ */
+function efParserFunctionsWords( $lang ) {
+	$words = array();
+	
+	/**
+	 * English
+	 */
+	$words['en'] = array(
+		'expr' 		=> array( 0, 'expr' ),
+		'if' 		=> array( 0, 'if' ),
+		'ifeq' 		=> array( 0, 'ifeq' ),
+		'ifexpr' 	=> array( 0, 'ifexpr' ),
+		'switch' 	=> array( 0, 'switch' ),
+		'default' 	=> array( 0, '#default' ),
+		'ifexist' 	=> array( 0, 'ifexist' ),
+		'time' 		=> array( 0, 'time' ),
+		'rel2abs' 	=> array( 0, 'rel2abs' ),
+		'titleparts' => array( 0, 'titleparts' ),
+	);
+	
+	/**
+	 * Farsi-Persian
+	 */
+	$words['fa'] = array(
+		'expr' 		=> array( 0, 'حساب',         'expr' ),
+		'if' 		=> array( 0, 'اگر',          'if' ),
+		'ifeq' 		=> array( 0, 'اگرمساوی',     'ifeq' ),
+		'ifexpr' 	=> array( 0, 'اگرحساب',      'ifexpr' ),
+		'switch' 	=> array( 0, 'گزینه',        'switch' ),
+		'default' 	=> array( 0, '#پیش‌فرض',      '#default' ),
+		'ifexist' 	=> array( 0, 'اگرموجود',     'ifexist' ),
+		'time' 		=> array( 0, 'زمان',         'time' ),
+		'rel2abs' 	=> array( 0, 'نسبی‌به‌مطلق',   'rel2abs' ),
+	);
+
+	/**
+	 * Hebrew
+	 */
+	$words['he'] => array(
+		'expr' 		=> array( 0, 'חשב',         'expr' ),
+		'if' 		=> array( 0, 'תנאי',        'if' ),
+		'ifeq' 		=> array( 0, 'שווה',        'ifeq' ),
+		'ifexpr' 	=> array( 0, 'חשב תנאי',    'ifexpr' ),
+		'switch' 	=> array( 0, 'בחר',         'switch' ),
+		'default' 	=> array( 0, '#ברירת מחדל', '#default' ),
+		'ifexist' 	=> array( 0, 'קיים',         'ifexist' ),
+		'time' 		=> array( 0, 'זמן',          'time' ),
+		'rel2abs' 	=> array( 0, 'יחסי למוחלט',  'rel2abs' ),
+		'titleparts' => array( 0, 'חלק בכותרת', 'titleparts' ),
+	);	
+
+	# English is used as a fallback, and the English synonyms are
+	# used if a translation has not been provided for a given word
+	return ( $lang == 'en' || !isset( $words[$lang] ) )
+		? $words['en']
+		: array_merge( $words['en'], $words[$lang] );
+}
+
+?>
