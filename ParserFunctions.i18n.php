bb783570809c17b55c7d90323613ca53368c9c82
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-03 22:12:16 +0000

    Localisation updates for extension messages from Betawiki (2008-02-03 22:50 CET)

--- ParserFunctions.i18n.php
+++ ParserFunctions.i18n.php
@@ -621,11 +621,23 @@ $messages['ps'] = array(
  * @author Malafaya
  */
 $messages['pt'] = array(
-	'pfunc_time_error'            => 'Erro: tempo inválido',
-	'pfunc_time_too_long'         => 'Erro: demasiadas chamadas a #time',
-	'pfunc_expr_division_by_zero' => 'Divisão por zero',
-	'pfunc_expr_not_a_number'     => 'Em $1: resultado não é um número',
-	'pfunc_max_ifexist_category'  => "Páginas com demasiadas chamadas 'ifexist'",
+	'pfunc_time_error'                      => 'Erro: tempo inválido',
+	'pfunc_time_too_long'                   => 'Erro: demasiadas chamadas a #time',
+	'pfunc_rel2abs_invalid_depth'           => 'Erro: Profundidade inválida no caminho: "$1" (foi tentado o acesso a um nó acima do nó raiz)',
+	'pfunc_expr_stack_exhausted'            => 'Erro de expressão: Pilha esgotada',
+	'pfunc_expr_unexpected_number'          => 'Erro de expressão: Número inesperado',
+	'pfunc_expr_preg_match_failure'         => 'Erro de expressão: Falha em preg_match inesperada',
+	'pfunc_expr_unrecognised_word'          => 'Erro de expressão: Palavra "$1" não reconhecida',
+	'pfunc_expr_unexpected_operator'        => 'Erro de expressão: Operador $1 inesperado',
+	'pfunc_expr_missing_operand'            => 'Erro de expressão: Falta operando para $1',
+	'pfunc_expr_unexpected_closing_bracket' => 'Erro de expressão: Parêntese de fecho inesperado',
+	'pfunc_expr_unrecognised_punctuation'   => 'Erro de expressão: Caracter de pontuação "$1" não reconhecido',
+	'pfunc_expr_unclosed_bracket'           => 'Erro de expressão: Parêntese não fechado',
+	'pfunc_expr_division_by_zero'           => 'Divisão por zero',
+	'pfunc_expr_unknown_error'              => 'Erro de expressão: Erro desconhecido ($1)',
+	'pfunc_expr_not_a_number'               => 'Em $1: resultado não é um número',
+	'pfunc_ifexist_warning'                 => 'Aviso: Esta página contém demasiadas chamadas #ifexist. Deverá ter menos de $2, mas neste momento existem $1.',
+	'pfunc_max_ifexist_category'            => "Páginas com demasiadas chamadas 'ifexist'",
 );
 
 /** Russian (Русский)
