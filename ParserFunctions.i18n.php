e9fd964456ff7c2d8aa05efe4200a3469da8f78f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-03-21 13:31:10 +0000

    Updating credits (Bdanee -> Dani)

--- ParserFunctions.i18n.php
+++ ParserFunctions.i18n.php
@@ -450,7 +450,7 @@ $messages['hsb'] = array(
 );
 
 /** Hungarian (Magyar)
- * @author Bdanee
+ * @author Dani
  */
 $messages['hu'] = array(
 	'pfunc_desc'                            => 'Az értelmező kiegészítése logikai funkciókkal',
