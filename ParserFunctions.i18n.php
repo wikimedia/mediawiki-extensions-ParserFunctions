26bd2162de2edd335a6f27fe78da7535015d2b2f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-04-01 07:46:44 +0000

    Localisation updates for extension messages from Betawiki (2008-04-01 09:08 CEST)

--- ParserFunctions.i18n.php
+++ ParserFunctions.i18n.php
@@ -832,24 +832,25 @@ $messages['oc'] = array(
  * @author Siebrand
  */
 $messages['pl'] = array(
-	'pfunc_desc'                            => 'Rozszerza analizator składni o funkcje logiki',
+	'pfunc_desc'                            => 'Rozszerza analizator składni o funkcje logiczne',
 	'pfunc_time_error'                      => 'Błąd: niepoprawny czas',
-	'pfunc_time_too_long'                   => 'Błąd: za dużo wywołań funkcji #time',
+	'pfunc_time_too_long'                   => 'Błąd: zbyt wiele wywołań funkcji #time',
 	'pfunc_rel2abs_invalid_depth'           => 'Błąd: Nieprawidłowa głębokość w ścieżce: "$1" (próba dostępu do węzła powyżej korzenia)',
 	'pfunc_expr_stack_exhausted'            => 'Błąd w wyrażeniu: Stos wyczerpany',
-	'pfunc_expr_unexpected_number'          => 'Błąd w wyrażeniu: Niespodziewana liczba',
-	'pfunc_expr_preg_match_failure'         => 'Błąd w wyrażeniu: Niespodziewany błąd w preg_match',
+	'pfunc_expr_unexpected_number'          => 'Błąd w wyrażeniu: Nieoczekiwana liczba',
+	'pfunc_expr_preg_match_failure'         => 'Błąd w wyrażeniu: Nieoczekiwany błąd preg_match',
 	'pfunc_expr_unrecognised_word'          => 'Błąd w wyrażeniu: Nierozpoznane słowo "$1"',
 	'pfunc_expr_unexpected_operator'        => 'Błąd w wyrażeniu: Nieoczekiwany operator $1',
-	'pfunc_expr_missing_operand'            => 'Błąd w wyrażeniu: Brakujący operand dla $1',
+	'pfunc_expr_missing_operand'            => 'Błąd w wyrażeniu: Brak argumentu funkcji $1',
 	'pfunc_expr_unexpected_closing_bracket' => 'Błąd w wyrażeniu: Nieoczekiwany nawias zamykający',
 	'pfunc_expr_unrecognised_punctuation'   => 'Błąd w wyrażeniu: Nierozpoznany znak interpunkcyjny "$1"',
 	'pfunc_expr_unclosed_bracket'           => 'Błąd w wyrażeniu: Niedomknięty nawias',
 	'pfunc_expr_division_by_zero'           => 'Dzielenie przez zero',
 	'pfunc_expr_unknown_error'              => 'Błąd w wyrażeniu: Nieznany błąd ($1)',
 	'pfunc_expr_not_a_number'               => 'W $1: wynik nie jest liczbą',
-	'pfunc_ifexist_warning'                 => 'Uwaga: Ta strona zawiera zbyt wiele wywołań funkcji #ifexist. Nie ich może być więcej niż $2, a jest obecnie $1.',
-	'pfunc_max_ifexist_category'            => 'Strony ze zbyt dużą ilością wywołań ifexist',
+	'pfunc_ifexist_warning'                 => 'Uwaga: Ta strona zawiera zbyt wiele wywołań funkcji #ifexist.
+Nie może być ich więcej niż $2, obecnie jest $1.',
+	'pfunc_max_ifexist_category'            => 'Strony ze zbyt dużą ilością wywołań funkcji #ifexist',
 );
 
 /** Piemontèis (Piemontèis)
