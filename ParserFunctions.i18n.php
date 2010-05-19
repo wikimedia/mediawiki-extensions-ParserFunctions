75e08ddf25c9feb02e8de53cff9407c86e69a668
Raimond Spekking <raymond@users.mediawiki.org>
2010-05-19 19:25:55 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-05-19 19:15:00 UTC)

--- ParserFunctions.i18n.php
+++ ParserFunctions.i18n.php
@@ -1592,25 +1592,25 @@ $messages['qu'] = array(
  * @author Stelistcristi
  */
 $messages['ro'] = array(
-	'pfunc_desc' => 'Îmbunătăţiţi parser-ul cu funcţii logice',
+	'pfunc_desc' => 'Îmbunătățiți parser-ul cu funcții logice',
 	'pfunc_time_error' => 'Eroare: timp incorect',
 	'pfunc_time_too_long' => 'Eroare: prea multe apeluri #time',
 	'pfunc_rel2abs_invalid_depth' => 'Eroare: adâncime incorectă în cale: "$1" (încercat accesarea unui nod deasupra nodului rădăcină)',
 	'pfunc_expr_stack_exhausted' => 'Eroare de expresie: Stivă epuizată',
-	'pfunc_expr_unexpected_number' => 'Eroare de expresie: număr neaşteptat',
-	'pfunc_expr_preg_match_failure' => 'Eroare de expresie: eşuare preg_match neaşteptată',
+	'pfunc_expr_unexpected_number' => 'Eroare de expresie: număr neașteptat',
+	'pfunc_expr_preg_match_failure' => 'Eroare de expresie: eșuare preg_match neașteptată',
 	'pfunc_expr_unrecognised_word' => 'Eroare de expresie: "$1" este cuvânt necunoscut',
-	'pfunc_expr_unexpected_operator' => 'Eroare de expresie: operator $1 neaşteptat',
+	'pfunc_expr_unexpected_operator' => 'Eroare de expresie: operator $1 neașteptat',
 	'pfunc_expr_missing_operand' => 'Eroare de expresie: operand lipsă pentru $1',
-	'pfunc_expr_unexpected_closing_bracket' => 'Eroare de expresie: paranteză închisă neaşteptată',
-	'pfunc_expr_unrecognised_punctuation' => 'Eroare de expresie: caracter de punctuaţie "$1" necunoscut',
+	'pfunc_expr_unexpected_closing_bracket' => 'Eroare de expresie: paranteză închisă neașteptată',
+	'pfunc_expr_unrecognised_punctuation' => 'Eroare de expresie: caracter de punctuație "$1" necunoscut',
 	'pfunc_expr_unclosed_bracket' => 'Eroare de expresie: paranteză neînchisă',
-	'pfunc_expr_division_by_zero' => 'Împărţire la zero',
+	'pfunc_expr_division_by_zero' => 'Împărțire la zero',
 	'pfunc_expr_invalid_argument' => 'Argument incorect pentru $1: < -1 sau > 1',
 	'pfunc_expr_invalid_argument_ln' => 'Argument incorect pentru ln: <= 0',
 	'pfunc_expr_unknown_error' => 'Eroare de expresie: eroare necunoscută ($1)',
 	'pfunc_expr_not_a_number' => 'În $1: rezultatul nu este un număr',
-	'pfunc_string_too_long' => 'Eroare: Şirul depăşeşte limita de caractere de $1',
+	'pfunc_string_too_long' => 'Eroare: Şirul depășește limita de caractere de $1',
 );
 
 /** Tarandíne (Tarandíne)
