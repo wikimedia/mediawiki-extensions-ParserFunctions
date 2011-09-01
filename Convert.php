86c86a376635d0cf0ab3498b999b38bc0716b7d0
Platonides <platonides@users.mediawiki.org>
2011-09-01 18:45:53 +0000

    Add the position of the text to the messages so that the position can be changed depending on the plural, as requested by Amir in http://lists.wikimedia.org/pipermail/mediawiki-i18n/2011-August/000338.html
    Placing the text after the unit could get funny in cases where there is
    more text than just the number and the unit, but it's not a problem for the 
    parserfunction to fix.
    
    The transformations for ParserFunctions.i18n.php were
    On pfunc-convert-unit messages: s/$2/$3/, s/$1/$2/
    s/'pfunc-convert-unit-.*' *=> '/\0$1/

--- Convert.php
+++ Convert.php
@@ -202,13 +202,13 @@ class ConvertParser {
 		if( $this->raw ){
 			return trim( $string );
 		} else {
-			$unit = $this->targetUnit->getText(
+			return $this->targetUnit->getText(
+				$string,
 				$this->lastValue,
 				$this->link,
 				$this->abbreviate,
 				$this->language
 			);
-			return  $string . $unit;
 		}
 	}
 
@@ -736,12 +736,13 @@ class ConvertUnit {
 
 	/**
 	 * Get the text of the unit
+	 * @param $string String Original text, with the number converted
 	 * @param $value String number for PLURAL support
 	 * @param $link Bool
 	 * @param $language Language
 	 * @return String
 	 */
-	public function getText( $value, $link=false, $abbreviate=false, $language=null ){
+	public function getText( $string, $value, $link=false, $abbreviate=false, $language=null ){
 		global $wgContLang;
 		$value = $wgContLang->formatNum( $value );
 
@@ -750,7 +751,7 @@ class ConvertUnit {
 				$this->dimension->getName(),
 				$this->unitName,
 				$this->prefix,
-				$value, $link, $abbreviate, $language
+				$string, $value, $link, $abbreviate, $language
 			);
 
 		} elseif( !wfEmptyMsg( "pfunc-convert-unit-{$this->dimension->getName(true)}-{$this->unitName[0]}-{$this->unitName[1]}" ) ){
@@ -761,7 +762,7 @@ class ConvertUnit {
 				$this->dimension->getName(true),
 				"{$this->unitName[0]}-{$this->unitName[1]}",
 				$this->prefix, # This will probably be rubbish, but it's the wiki users' problem, not ours
-				$value, $link, $abbreviate, $language
+				$string, $value, $link, $abbreviate, $language
 			);
 
 		} else {
@@ -770,12 +771,13 @@ class ConvertUnit {
 				$dimensionNames[0],
 				$this->unitName[0],
 				$this->prefix[0],
-				$value, $link, $abbreviate, $language
+				$string, $value, $link, $abbreviate, $language
 			);
 			$msg2Text = $this->getTextFromMessage(
 				$dimensionNames[1],
 				$this->unitName[1],
 				$this->prefix[1],
+				'',
 				1, # Singular for denominator
 				$link, $abbreviate, $language
 			);
@@ -785,7 +787,7 @@ class ConvertUnit {
 		return trim( $msgText );
 	}
 
-	protected function getTextFromMessage( $dimension, $unit, $prefix, $number, $link, $abbreviate, $language ){
+	protected function getTextFromMessage( $dimension, $unit, $prefix, $string, $number, $link, $abbreviate, $language ){
 		$abbr = $abbreviate ? '-abbr' : '';
 		$prefix = $prefix === null
 			? ''
@@ -794,6 +796,7 @@ class ConvertUnit {
 		$text = wfMsgExt(
 			"pfunc-convert-unit-$dimension-$unit$abbr",
 			array( 'parsemag', 'language' => $language ),
+			$string,
 			$number,
 			$prefix
 		);
