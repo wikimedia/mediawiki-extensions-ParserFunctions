a76aa995fe4036005ce804dbd80fbdb071c4b15a
Sean Colombo <sean_colombo@users.mediawiki.org>
2011-11-21 21:18:18 +0000

    ParserFunctions: The third param turned out to have the same issue as the second (was throwing warnings in production since empty-strings were making their way in).  Followup to r103834.

--- ParserFunctions_body.php
+++ ParserFunctions_body.php
@@ -693,8 +693,7 @@ class ExtParserFunctions {
 		if ( intval( $inLength ) == 0 ) {
 			$result = mb_substr( $inStr, $inStart );
 		} else {
-			$inStart = intval( $inStart ); // prevents warnings when inStart is an empty string
-			$result = mb_substr( $inStr, $inStart, $inLength );
+			$result = mb_substr( $inStr, intval( $inStart ), intval( $inLength ) );
 		}
 
 		wfProfileOut( __METHOD__ );
