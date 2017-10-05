a95d02548bc6fe722cacd1845c9ce86400a98adb
Addshore <addshorewiki@gmail.com>
2017-10-05 17:27:11 +0000

    Revert "Language objects aren't stubbed anymore"
    This reverts commit 86b387409c4cb50458ecb9d71c6f34203f6d810c.
    
    Bug: T177478
    Change-Id: I14b81d8fd85c1398c5fa7544d48c941c4e4f9c5a

--- ParserFunctions_body.php
+++ ParserFunctions_body.php
@@ -480,6 +480,8 @@ class ExtParserFunctions {
 					} else {
 						// use wiki's content language
 						$langObject = $parser->getFunctionLang();
+						// $ttl is passed by reference, which doesn't work right on stub objects
+						StubObject::unstub( $langObject );
 					}
 					$result = $langObject->sprintfDate( $format, $ts, $tz, $ttl );
 				} else {
