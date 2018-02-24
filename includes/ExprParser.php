9f94cebdaaf4e830aa79043a0ed1439091209410
Kunal Mehta <legoktm@member.fsf.org>
2018-02-24 14:06:58 -0800

    Move classes to includes/
    Change-Id: Iefaa21bf911fa7dcf11aedd38648bed945b905f9

--- includes/ExprParser.php
+++ includes/ExprParser.php
@@ -1,10 +1,22 @@
 <?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 use UtfNormal\Validator;
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
-}
 
 // Character classes
 define( 'EXPR_WHITE_CLASS', " \t\r\n" );
@@ -49,22 +61,6 @@ define( 'EXPR_POW', 35 );
 define( 'EXPR_PI', 36 );
 define( 'EXPR_FMOD', 37 );
 define( 'EXPR_SQRT', 38 );
-
-class ExprError extends Exception {
-	/**
-	 * @param string $msg
-	 * @param string $parameter
-	 */
-	public function __construct( $msg, $parameter = '' ) {
-		// Give grep a chance to find the usages:
-		// pfunc_expr_stack_exhausted, pfunc_expr_unexpected_number, pfunc_expr_preg_match_failure,
-		// pfunc_expr_unrecognised_word, pfunc_expr_unexpected_operator, pfunc_expr_missing_operand,
-		// pfunc_expr_unexpected_closing_bracket, pfunc_expr_unrecognised_punctuation,
-		// pfunc_expr_unclosed_bracket, pfunc_expr_division_by_zero, pfunc_expr_invalid_argument,
-		// pfunc_expr_invalid_argument_ln, pfunc_expr_unknown_error, pfunc_expr_not_a_number
-		$this->message = wfMessage( "pfunc_expr_$msg", $parameter )->inContentLanguage()->text();
-	}
-}
 
 class ExprParser {
 	public $maxStackSize = 100;
