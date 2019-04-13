cca4ec9af01dd89cab09beee0bf4a7716dc0a406
Max Semenik <maxsem.wiki@gmail.com>
2019-04-12 22:25:14 -0700

    Remove function existence check
    PPFrame::setTTL() is present in every supported MediaWiki version.
    
    Change-Id: I7065723bfbd2170cd7c951d1f9b3c4a0318f9938

--- includes/ParserFunctions.php
+++ includes/ParserFunctions.php
@@ -420,9 +420,7 @@ class ParserFunctions {
 		}
 		if ( isset( self::$mTimeCache[$format][$cacheKey][$language][$local] ) ) {
 			$cachedVal = self::$mTimeCache[$format][$cacheKey][$language][$local];
-			if ( $useTTL
-				&& $cachedVal[1] !== null && $frame && is_callable( [ $frame, 'setTTL' ] )
-			) {
+			if ( $useTTL && $cachedVal[1] !== null && $frame ) {
 				$frame->setTTL( $cachedVal[1] );
 			}
 			return $cachedVal[0];
@@ -504,7 +502,7 @@ class ParserFunctions {
 			}
 		}
 		self::$mTimeCache[$format][$cacheKey][$language][$local] = [ $result, $ttl ];
-		if ( $useTTL && $ttl !== null && $frame && is_callable( [ $frame, 'setTTL' ] ) ) {
+		if ( $useTTL && $ttl !== null && $frame ) {
 			$frame->setTTL( $ttl );
 		}
 		return $result;
