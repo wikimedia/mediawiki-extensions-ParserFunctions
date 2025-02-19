5908adda1850a7e59d1d0c7c08476a8aefc3d7de
Ed Sanders <esanders@wikimedia.org>
2025-02-19 15:55:08 +0000

    ESLint: Use server config for server files
    Change-Id: I73bfc549fccf6d09589ea9e97d517a3a8213e518

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,10 @@
  * @package ParserFunctions
  */
 
-/* eslint-env node, es6 */
+'use strict';
+
 module.exports = function ( grunt ) {
-	var conf = grunt.file.readJSON( 'extension.json' );
+	const conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
@@ -15,12 +16,10 @@ module.exports = function ( grunt ) {
 		banana: conf.MessagesDirs,
 		eslint: {
 			options: {
-				cache: true
+				cache: true,
+				fix: grunt.option( 'fix' )
 			},
-			all: [
-				'**/*.js{,on}',
-				'!{vendor,node_modules}/**'
-			]
+			all: [ '.' ]
 		}
 	} );
 
